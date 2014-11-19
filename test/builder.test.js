61b6a35a92179eb7a5b3b36cde3cd35d87fa543b
Enrico Stara <enrico.stara@gmail.com>
2014-11-19 18:22:53 +0100

    Remove the dependency by `mtproto` module
    - code review
    
    - add unit-test for Type function creation

--- test/builder.test.js
+++ test/builder.test.js
@@ -7,35 +7,32 @@ describe('Builder', function () {
 
     describe('#buildTypeConstructor({P_Q_inner_data})', function () {
         it('should return a P_Q_inner_data', function (done) {
-            var P_Q_inner_data = new Builder({module: 'mtproto', tlSchema:{"id": "-2083955988", "predicate": "p_q_inner_data", "params": [
+            var P_Q_inner_data = new Builder('namespace', {"id": "-2083955988", "predicate": "p_q_inner_data", "params": [
                 {"name": "pq", "type": "bytes"},
                 {"name": "p", "type": "bytes"},
                 {"name": "q", "type": "bytes"},
                 {"name": "nonce", "type": "int128"},
                 {"name": "server_nonce", "type": "int128"},
                 {"name": "new_nonce", "type": "int256"}
-            ], "type": "P_Q_inner_data"}}).getType();
-//            console.log(P_Q_inner_data.toString());
+            ], "type": "P_Q_inner_data"}).getType();
             P_Q_inner_data.should.be.an.instanceof(Function);
             var obj = new P_Q_inner_data();
             obj.should.be.an.instanceof(P_Q_inner_data);
             obj.should.be.an.instanceof(AbstractObject);
             obj.id.should.be.eql('ec5ac983');
-            obj.typeName.should.be.eql('mtproto.P_q_inner_data');
-
+            obj.typeName.should.be.eql('namespace.P_q_inner_data');
             done();
         })
     });
 
     describe('#buildTypeConstructor({ResPQ}).deserialize()', function () {
         it('should build and de-serialize an instance of ResPQ', function (done) {
-            var ResPQ = new Builder({module: 'mtproto',tlSchema: {"id": "85337187", "predicate": "resPQ", "params": [
+            var ResPQ = new Builder('namespace', {"id": "85337187", "predicate": "resPQ", "params": [
                 {"name": "nonce", "type": "int128"},
                 {"name": "server_nonce", "type": "int128"},
                 {"name": "pq", "type": "bytes"},
                 {"name": "server_public_key_fingerprints", "type": "Vector<long>"}
-            ], "type": "ResPQ"}}).getType();
-//            console.log(ResPQ.toString());
+            ], "type": "ResPQ"}).getType();
             ResPQ.should.be.an.instanceof(Function);
             var obj = new ResPQ({buffer: new Buffer(
                 '632416053E0549828CCA27E966B301A48FECE2FCA5CF4D33F4A11EA877BA4AA5739073300817ED48941A08F98100000015C4B51C01000000216BE86C022BB4C3',
@@ -43,10 +40,9 @@ describe('Builder', function () {
             obj.should.be.an.instanceof(ResPQ);
             obj.should.be.an.instanceof(AbstractObject);
             obj.deserialize();
-//            console.log(obj);
             obj.should.have.properties({
                 id: '63241605',
-                typeName: 'mtproto.ResPQ',
+                typeName: 'namespace.ResPQ',
                 nonce: '0xfce2ec8fa401b366e927ca8c8249053e',
                 server_nonce: '0x30739073a54aba77a81ea1f4334dcfa5'
             });
@@ -61,17 +57,13 @@ describe('Builder', function () {
 
     describe('#buildTypeConstructor({ResPQ}).serialize()', function () {
         it('should build and serialize an instance of ResPQ', function (done) {
-            var ResPQ = new Builder({module: 'builder',tlSchema: {"id": "85337187", "predicate": "resPQ", "params": [
+            var ResPQ = new Builder('namespace', {"id": "85337187", "predicate": "resPQ", "params": [
                 {"name": "nonce", "type": "int128"},
                 {"name": "server_nonce", "type": "int128"},
                 {"name": "pq", "type": "bytes"},
                 {"name": "server_public_key_fingerprints", "type": "Vector<long>"}
-            ], "type": "ResPQ"}}).getType();
-
-//            console.log(ResPQ.toString());
-
+            ], "type": "ResPQ"}).getType();
             ResPQ.should.be.an.instanceof(Function);
-
             var obj = new ResPQ({props: {
                 nonce: '0xfce2ec8fa401b366e927ca8c8249053e',
                 server_nonce: '0x30739073a54aba77a81ea1f4334dcfa5',
@@ -81,12 +73,55 @@ describe('Builder', function () {
             });
             var objBuffer = obj.serialize();
             objBuffer.toString('hex').toUpperCase().should.be.
-                eql('632416053E0549828CCA27E966B301A48FECE2FCA5CF4D33F4A11EA877BA4AA5739073300817ED48941A08F98100000015C4B51C01000000216BE86C022BB4C3')
-
+                eql('632416053E0549828CCA27E966B301A48FECE2FCA5CF4D33F4A11EA877BA4AA5739073300817ED48941A08F98100000015C4B51C01000000216BE86C022BB4C3');
             var obj2 = new ResPQ({buffer: objBuffer});
             obj2.id.should.be.eql('63241605');
-
             done();
+        })
+    });
+
+    describe('#buildTypeFunction({reqPQ}).serialize()', function () {
+        it('should build a reqPQ Type function', function (done) {
+            var wrapperMessage = function WrapperMessage(param) {
+                this.serialize = function () {
+                    return param.message;
+                };
+                this.deserialize = function () {
+                    return {
+                        getMessage: function () {
+                            return param.buffer;
+                        }
+                    };
+                };
+            }
+            var reqPQ = new Builder('namespace', {"id": "1615239032", "method": "req_pq", "params": [
+                {"name": "nonce", "type": "int128"}
+            ], "type": "ResPQ"}, wrapperMessage).getType();
+            reqPQ.should.be.an.instanceof(Function);
+            var nonce = '0xf67b7768bf4854bb15fa840ec843875f';
+            var conn = {
+                connect: function (callback) {
+                    callback();
+                },
+                write: function (buffer, callback) {
+                    this.buffer = buffer;
+                    callback();
+                },
+                read: function (callback) {
+                    callback(null, this.buffer);
+                }
+            };
+            reqPQ({
+                props: {
+                    nonce: nonce
+                },
+                conn: conn,
+                callback: function (ex, response) {
+                    if (ex) console.warn(ex);
+                    response.nonce.should.be.eql(nonce);
+                    done();
+                }
+            });
         })
     });
 });
