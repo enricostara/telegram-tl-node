2f76f0b81de89287b551aa1ec15fd5ed025d9e5d
Enrico Stara <enrico.stara@gmail.com>
2014-11-19 11:39:18 +0100

    Add sources

---
+++ index.js
@@ -0,0 +1,9 @@
+//       telegram-tl-node
+//
+//       Copyright 2014 Enrico Stara 'enrico.stara@gmail.com'
+//       Released under the Simplified BSD License
+//       https://github.com/enricostara/telegram-tl-node
+
+exports.AbstractObject = require('./lib/abstract-object');
+exports.Builder = require('./lib/builder');
+exports.Vector = require('./lib/vector');
