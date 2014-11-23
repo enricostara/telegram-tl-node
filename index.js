a2c26f59c13bb7b238c133d7cf0712dc85973277
Enrico Stara <enrico.stara@gmail.com>
2014-11-23 19:28:33 +0100

    Update copyright

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-//       telegram-tl-node
-//
-//       Copyright 2014 Enrico Stara 'enrico.stara@gmail.com'
-//       Released under the Simplified BSD License
-//       https://github.com/enricostara/telegram-tl-node
+//     telegram-tl-node
+//     Copyright 2014 Enrico Stara 'enrico.stara@gmail.com'
+//     Released under the MIT License
+//     https://github.com/enricostara/telegram-tl-node
 
 exports.TypeBuilder = require('./lib/type-builder');
 exports.TypeObject = require('./lib/type-object');
