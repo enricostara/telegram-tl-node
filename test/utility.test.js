3a5711331f4edc18c5d858ca35c687d5145848a1
Enrico Stara <enrico.stara@gmail.com>
2015-03-22 21:11:13 +0100

    Add the array type to the toPrintable() method

--- test/utility.test.js
+++ test/utility.test.js
@@ -2,15 +2,21 @@ require('should');
 var TypeObject = require('../lib/type-object');
 var utility = require('../lib/utility');
 
-describe('utility', function() {
+describe('utility', function () {
 
-    describe('#toPrintable()', function() {
-        it('should return a printable ', function(done) {
+    describe('#toPrintable()', function () {
+        it('should return a printable ', function (done) {
             var obj = new TypeObject();
             obj.number = 1000;
             obj.exclude = 1000;
             obj.string = 'string value';
             obj.buffer = new Buffer('ffff', 'hex');
+            obj.array = ['abc', 'def'];
+            var arrayInnerObj1 = new TypeObject();
+            arrayInnerObj1.number = 100;
+            var arrayInnerObj2 = new TypeObject();
+            arrayInnerObj2.number = 200;
+            obj.array2 = [arrayInnerObj1, arrayInnerObj2];
             var innerObj = new TypeObject();
             innerObj.number = 2000;
             innerObj.exclude = 2000;
@@ -18,11 +24,15 @@ describe('utility', function() {
             innerObj.buffer = new Buffer('eeeeee', 'hex');
             obj.innerObj = innerObj;
 
-            obj.toPrintable(true).should.be.equal('{ number: 1000, exclude: 1000, string: "string value", buffer[2]: 0xffff, innerObj: { number: 2000, exclude: 2000, innerExclude: 2000, buffer[3]: 0xeeeeee } }');
+            obj.toPrintable(true).should.be.equal('{ number: 1000, exclude: 1000, string: "string value", buffer[2]: 0xffff, ' +
+            'array: ["abc", "def"], array2: [{ number: 100 }, { number: 200 }], ' +
+            'innerObj: { number: 2000, exclude: 2000, innerExclude: 2000, buffer[3]: 0xeeeeee } }');
             obj.toPrintable({
                 exclude: true,
                 innerObj: {innerExclude: true}
-            }, true).should.be.equal('{ number: 1000, string: "string value", buffer[2]: 0xffff, innerObj: { number: 2000, exclude: 2000, buffer[3]: 0xeeeeee } }');
+            }, true).should.be.equal('{ number: 1000, string: "string value", buffer[2]: 0xffff, ' +
+                'array: ["abc", "def"], array2: [{ number: 100 }, { number: 200 }], ' +
+                'innerObj: { number: 2000, exclude: 2000, buffer[3]: 0xeeeeee } }');
             console.log(obj.toPrintable({
                 exclude: true,
                 innerObj: {innerExclude: true}
