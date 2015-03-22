ba93d4e6a648ba2a42b4e4bd3c97388f8b437bbc
Enrico Stara <enrico.stara@gmail.com>
2015-03-22 21:18:44 +0100

    Set the '_readOffset' value when deserialise an object property

--- test/type-vector.test.js
+++ test/type-vector.test.js
@@ -75,52 +75,71 @@ describe('TypeVector', function() {
         "type": "Message"
     }).getType();
 
-    var Body2 = new TypeBuilder('namespace', {
+    var Body = new TypeBuilder('namespace', {
         "id": "66667",
-        "predicate": "Body2",
+        "predicate": "Body",
         "params": [
             {
                 "name": "key",
                 "type": "int"
             }
         ],
-        "type": "Body2"
+        "type": "Body"
     }).getType();
 
     describe('#serialize()', function() {
         it('should serialize the list with a bare Message ', function(done) {
-            var message = new Message({
+            var message1 = new Message({
                 props: {
                     msg_id: '1',
                     seqno: 1,
-                    body: new Body2({
+                    body: new Body({
                         props: {
                             key: 6666
                         }
                     })
                 }
             });
-            var list = new TypeVector({type: '%Message', list: [message]});
+            var message2 = new Message({
+                props: {
+                    msg_id: '2',
+                    seqno: 3,
+                    body: new Body({
+                        props: {
+                            key: 7777
+                        }
+                    })
+                }
+            });
+            var list = new TypeVector({type: '%Message', list: [message1, message2]});
             var buffer = list.serialize();
             buffer.should.be.ok;
-            buffer.toString('hex').should.be.equal('15c4b51c01000000010000000000000001000000080000006b0401000a1a0000');
+            buffer.toString('hex').should.be.equal('15c4b51c02000000010000000000000001000000080000006b0401000a1a0000020000000000000003000000080000006b040100611e0000');
             done();
         })
     });
 
     describe('#deserialize()', function() {
         it('should de-serialize the list with a bare Message', function(done) {
-            var list = new TypeVector({type: '%Message', buffer: new Buffer('15c4b51c01000000010000000000000001000000080000006b0401000a1a0000', 'hex')});
+            var list = new TypeVector({type: '%Message', buffer: new Buffer('15c4b51c02000000010000000000000001000000080000006b0401000a1a0000020000000000000003000000080000006b040100611e0000', 'hex')});
             list.deserialize().should.be.ok;
-            list.getList().length.should.be.equal(1);
+            list.getList().length.should.be.equal(2);
 
-            var message = list.getList().pop();
+            var message = list.getList()[0];
             message.should.have.properties({
                 msg_id: '0x0000000000000001',
                 seqno: 1,
                 bytes: 8
             });
             message.body.key.should.be.equal(6666);
+
+            message = list.getList()[1];
+            message.should.have.properties({
+                msg_id: '0x0000000000000002',
+                seqno: 3,
+                bytes: 8
+            });
+            message.body.key.should.be.equal(7777);
             done();
         })
     });
